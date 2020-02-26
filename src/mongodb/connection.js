0581874847e895942a473a8b874202813f738fc2
unknown <chinmayvib@gmail.com>
2020-02-27 03:51:21 +0530

    bugfix

--- src/mongodb/connection.js
+++ src/mongodb/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const product = require("./models");
-const connection = process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/product-info-scraper";
+const connection = process.env.MONGODB_URI || "mongodb://mongo:27017/product-info-scraper";
 const connectDb = () => {
 	return mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true });
 };
